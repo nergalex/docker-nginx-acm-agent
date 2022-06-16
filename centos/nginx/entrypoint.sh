65746774ebcc21ded6002dbc0530b421bc7afeb1
Alexis <al.dacosta@f5.com>
2022-06-17 00:05:43 +0200

    nginx

--- centos/nginx/entrypoint.sh
+++ centos/nginx/entrypoint.sh
@@ -8,18 +8,11 @@ echo "------ version 2022.06.16.01 ------"
 agent_conf_file="/etc/nginx-agent/nginx-agent.conf"
 agent_log_file="/var/log/nginx-agent/agent.log"
 nginx_status_conf="/etc/nginx/conf.d/stub_status.conf"
-#api_key=""
-#instance_name="$(hostname -f)"
 controller_host=""
-#location=""
 
 handle_term()
 {
     echo "received TERM signal"
-    echo "stopping bd-socket-plugin"
-    kill -TERM "${bd_socket_pid}" 2>/dev/null
-    echo "stopping bd-agent"
-    kill -TERM "${bd_agent_pid}" 2>/dev/null
     echo "stopping nginx-agent ..."
     kill -TERM "${agent_pid}" 2>/dev/null
     echo "stopping nginx ..."
@@ -28,11 +21,6 @@ handle_term()
 
 trap 'handle_term' TERM
 
-# Launch NAP
-/bin/su -s /bin/bash -c '/opt/app_protect/bin/bd_agent &' nginx
-bd_agent_pid=$(pgrep bd_agent)
-/bin/su -s /bin/bash -c "/usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 307200000 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config 2>&1 > /var/log/app_protect/bd-socket-plugin.log &" nginx
-bd_socket_pid=$(pgrep bd-socket)
 # Launch nginx
 echo "starting nginx ..."
 nginx -g "daemon off;" &
@@ -49,18 +37,8 @@ wait_workers()
 
 wait_workers
 
-#test -n "${ENV_CONTROLLER_API_KEY}" && \
-#    api_key=${ENV_CONTROLLER_API_KEY}
-
-# if instance_name is defined in the env vars, use it
-#test -n "${ENV_CONTROLLER_INSTANCE_NAME}" && \
-#    instance_name=${ENV_CONTROLLER_INSTANCE_NAME}
-
 test -n "${ENV_CONTROLLER_HOST}" && \
     controller_host=${ENV_CONTROLLER_HOST}
-
-#test -n "${ENV_CONTROLLER_LOCATION}" && \
-#    location=${ENV_CONTROLLER_LOCATION}
 
 test -n "${ENV_CONTROLLER_INSTANCE_GROUP}" && \
     instance_group=${ENV_CONTROLLER_INSTANCE_GROUP}
@@ -74,34 +52,14 @@ if [ -n "${controller_host}" -o -n "${instance_group}" ]; then
       { echo "no ${agent_conf_file}.default found! exiting."; exit 1; }
     fi
 
-#    test -n "${api_key}" && \
-#    echo " ---> using api_key = ${api_key}" && \
-#    sh -c "sed -i.old -e 's/api_key.*$/api_key = $api_key/' \
-#	${agent_conf_file}"
-
     test -n "${controller_host}" && \
     echo " ---> using controller api url = ${controller_host}" && \
     sh -c "sed -i.old -e 's@^\s\shost:\s.*@  host: $controller_host@' \
 	${agent_conf_file}"
 
-#    test -n "${instance_name}" && \
-#    echo " ---> using instance_name = ${instance_name}" && \
-#    sh -c "sed -i.old -e 's/instance_name.*$/instance_name = $instance_name/' \
-#	${agent_conf_file}"
-
-#    test -n "${location}" && \
-#    echo " ---> using location = ${location}" && \
-#    sh -c "sed -i.old -e 's/location_name.*$/location_name = $location/' \
-#	${agent_conf_file}"
-
     test -n "${instance_group}" && \
     echo " ---> using instance group = ${instance_group}" && \
     sh ./install.sh -g ${instance_group}
-
-#    if ! grep -Fq "security = " ${agent_conf_file}; then
-#    sh -c "sed -i.old -e 's/\[extensions\]/&\nsecurity = True/g' \
-#        ${agent_conf_file}"
-#    fi
 
     test -f "${agent_conf_file}" && \
     chmod 644 ${agent_conf_file} && \
@@ -111,11 +69,6 @@ if [ -n "${controller_host}" -o -n "${instance_group}" ]; then
     chmod 644 ${nginx_status_conf} && \
     chown nginx ${nginx_status_conf} > /dev/null 2>&1
 fi
-
-#if ! grep '^api_key.*=[ ]*[[:alnum:]].*' ${agent_conf_file} > /dev/null 2>&1; then
-#    echo "no api_key found in ${agent_conf_file}! exiting."
-#    exit 1
-#fi
 
 echo "starting controller-agent ..."
 /usr/bin/nginx-agent > /dev/null 2>&1 < /dev/null &
