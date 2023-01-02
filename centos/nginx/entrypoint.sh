5c2d83478855854fc9d1bc5cbce0c0ba46c400ce
Alexis <al.dacosta@f5.com>
2023-01-02 01:10:44 +0100

    agent log debug

--- centos/nginx/entrypoint.sh
+++ centos/nginx/entrypoint.sh
@@ -64,7 +64,7 @@ fi
 
 if [ -n "${instance_group}" ]; then
   echo "starting nginx-agent with instance group ${instance_group}..."
-  /usr/bin/nginx-agent --instance-group ${instance_group} > /dev/null 2>&1 < /dev/null &
+  /usr/bin/nginx-agent --instance-group ${instance_group} &
 else
   echo "starting nginx-agent..."
   /usr/bin/nginx-agent > /dev/null 2>&1 < /dev/null &
