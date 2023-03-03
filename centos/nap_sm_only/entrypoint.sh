d19f4f7fd930349e8c075b671bf0ba293685bec4
Alexis <al.dacosta@f5.com>
2023-03-03 21:29:04 +0100

    listen 5144

--- centos/nap_sm_only/entrypoint.sh
+++ centos/nap_sm_only/entrypoint.sh
@@ -2,7 +2,7 @@
 #
 # This script launches nginx and the NGINX Controller Agent.
 #
-echo "------ version 2022.12.30.01 ------"
+echo "------ version 2023.03.03.01 ------"
 
 # Variables
 agent_conf_file="/etc/nginx-agent/nginx-agent.conf"
