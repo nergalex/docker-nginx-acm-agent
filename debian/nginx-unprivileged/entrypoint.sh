5ea2fba1f63eb0bef6c34bf03f580e9bb0d98920
Alexis <al.dacosta@f5.com>
2023-05-04 23:13:33 +0200

    clean

--- debian/nginx-unprivileged/entrypoint.sh
+++ debian/nginx-unprivileged/entrypoint.sh
@@ -6,8 +6,6 @@ echo "------ version 2023.05.04.02 ------"
 
 install_path="/nginx"
 nginx_config_path="/nginx-config"
-# agent_conf_file="/etc/nginx-agent/nginx-agent.conf"
-# agent_log_file="${install_path}/var/log/nginx-agent/agent.log"
 
 handle_term()
 {
@@ -18,12 +16,8 @@ handle_term()
 
 trap 'handle_term' TERM
 
-# Copy initial nginx config to target directory
-mkdir -p ${nginx_config_path}/etc/nginx
+# Copy initial nginx config to empty volume
 cp ${install_path}/etc/nginx/nginx.conf ${nginx_config_path}/nginx.conf
-chmod g+w ${nginx_config_path}/nginx.conf
-# ln -s  ${nginx_config_path}/etc/nginx/nginx.conf ${install_path}/etc/nginx/nginx.conf
-# ln -s  /nginx/usr/lib/nginx/modules/ ${nginx_config_path}/etc/nginx/modules
 
 # Launch nginx
 echo "starting nginx ..."
@@ -42,42 +36,7 @@ wait_workers()
 wait_workers
 
 # Launch nginx-agent
-#echo "updating ${agent_conf_file} ..."
-#
-#if [ ! -f "${agent_conf_file}" ]; then
-#  test -f "${agent_conf_file}.default" && \
-#  cp -p "${agent_conf_file}.default" "${agent_conf_file}" || \
-#  { echo "no ${agent_conf_file}.default found! exiting."; exit 1; }
-#fi
-#
-#test -n "${ENV_CONTROLLER_HOST}" && \
-#echo " ---> using controller api url = ${ENV_CONTROLLER_HOST}" && \
-#sh -c "sed -i.old -e 's@^\s\shost:\s.*@  host: ${ENV_CONTROLLER_HOST}@' \
-#${agent_conf_file}"
-#
-#test -f "${agent_conf_file}" && \
-#chmod 644 ${agent_conf_file} && \
-#chown nginx ${agent_conf_file} > /dev/null 2>&1
-
-# echo "starting nginx-agent with instance group ${ENV_CONTROLLER_INSTANCE_GROUP} and host ${ENV_CONTROLLER_HOST} ..."
 /usr/bin/nginx-agent &
-#  --instance-group ${ENV_CONTROLLER_INSTANCE_GROUP} \
-#  --server-host ${ENV_CONTROLLER_HOST} \
-#  --server-grpcport 443 \
-#  --tls-enable \
-#  --tls-skip-verify \
-#  --log-level info \
-#  --log-path ${install_path}/var/log/nginx-agent/ \
-#  --nginx-exclude-logs "" \
-#  --nginx-socket "unix:/var/run/nginx-agent/nginx.sock" \
-#  --dataplane-status-poll-interval 30s \
-#  --dataplane-report-interval 24h \
-#  --metrics-bulk-size 20 \
-#  --metrics-report-interval 1m \
-#  --metrics-collection-interval 15s \
-#  --metrics-mode aggregated \
-#  --config-dirs "${install_path}/etc/nginx:${install_path}/etc/nginx/modules" \
-#  &
 
 agent_pid=$!
 
