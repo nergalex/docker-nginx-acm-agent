73a4e3ddd43cf0eca8e5cb9b86d00119d4e0767a
Alexis <al.dacosta@f5.com>
2023-05-04 22:50:23 +0200

    config a la racine

--- debian/nginx-unprivileged/entrypoint.sh
+++ debian/nginx-unprivileged/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# This script launches nginx and OIDC module.
+# This script launches nginx and nginx-agent
 #
 echo "------ version 2023.05.04.02 ------"
 
@@ -20,14 +20,14 @@ trap 'handle_term' TERM
 
 # Copy initial nginx config to target directory
 mkdir -p ${nginx_config_path}/etc/nginx
-cp ${install_path}/etc/nginx/nginx.conf ${nginx_config_path}/etc/nginx/nginx.conf
-chmod g+w -R ${nginx_config_path}/etc/nginx
+cp ${install_path}/etc/nginx/nginx.conf ${nginx_config_path}/nginx.conf
+chmod g+w ${nginx_config_path}/nginx.conf
 # ln -s  ${nginx_config_path}/etc/nginx/nginx.conf ${install_path}/etc/nginx/nginx.conf
 # ln -s  /nginx/usr/lib/nginx/modules/ ${nginx_config_path}/etc/nginx/modules
 
 # Launch nginx
 echo "starting nginx ..."
-${install_path}/usr/sbin/nginx -p ${install_path}/etc/nginx -c ${nginx_config_path}/etc/nginx/nginx.conf -g "daemon off; load_module modules/ngx_http_js_module.so;" &
+${install_path}/usr/sbin/nginx -p ${install_path}/etc/nginx -c ${nginx_config_path}/nginx.conf -g "daemon off; load_module modules/ngx_http_js_module.so;" &
 
 nginx_pid=$!
 
