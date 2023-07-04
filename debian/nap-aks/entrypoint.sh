f4ae17998355fc116512877f0d030d2026497656
Alexis <al.dacosta@f5.com>
2023-07-04 11:18:15 +0200

    nap acm aks

---
+++ debian/nap-aks/entrypoint.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+#
+# This script launches nginx and nginx-agent
+#
+echo "------ version 2023.07.03.4 ------"
+
+handle_term()
+{
+    echo "received TERM signal"
+    echo "stopping nginx ..."
+    kill -TERM "${nginx_pid}" 2>/dev/null
+}
+
+trap 'handle_term' TERM
+
+# Launch nginx app protect WAF
+echo "starting app protect waf ..."
+/bin/su -s /bin/sh -c "/usr/share/ts/bin/bd-socket-plugin tmm_count 4 proc_cpuinfo_cpu_mhz 2000000 total_xml_memory 307200000 total_umu_max_size 3129344 sys_max_account_id 1024 no_static_config 2>&1 >> /var/log/app_protect/bd-socket-plugin.log &" nginx
+
+# Launch nginx
+echo "starting nginx ..."
+/usr/sbin/nginx -p /etc/nginx -c /etc/nginx/nginx.conf -g "daemon off;" &
+
+nginx_pid=$!
+
+wait_workers()
+{
+    while ! pgrep -f 'nginx: worker process' >/dev/null 2>&1; do
+        echo "waiting for nginx workers ..."
+        sleep 2
+    done
+}
+
+wait_workers
+
+# Launch nginx-agent
+/usr/bin/nginx-agent &
+
+agent_pid=$!
+
+if [ $? != 0 ]; then
+    echo "couldn't start the agent, please check nginx-agent logs"
+    exit 1
+fi
+
+wait_term()
+{
+    wait ${agent_pid}
+    trap '' EXIT INT TERM
+    kill -QUIT "${nginx_pid}" 2>/dev/null
+    echo "waiting for nginx to stop..."
+    wait ${nginx_pid}
+    kill -TERM "${agent_pid}" 2>/dev/null
+    echo "terminating nginx-agent..."
+    # unregister - start
+    echo "UNREGISTER instance from NMS"
+    export ENV_CONTROLLER_USER=${ENV_CONTROLLER_USER}
+    export ENV_CONTROLLER_PASSWORD=${ENV_CONTROLLER_PASSWORD}
+    export ENV_CONTROLLER_HOST=${ENV_CONTROLLER_HOST}
+    export ENV_CONTROLLER_INSTANCE_GROUP=${ENV_CONTROLLER_INSTANCE_GROUP}
+    sleep 60
+    sh remove.sh
+    echo "UNREGISTER done"
+    # unregister - end
+}
+
+wait_term
+
+echo "nginx process has stopped, exiting."
