203770cc6f324610bb2782fe127f5f4bf8bdf0da
nergalex <nergalex@hotmail.com>
2025-06-27 19:28:24 +0200

    NAP config

--- debian/nap/entrypoint.sh
+++ debian/nap/entrypoint.sh
@@ -2,7 +2,7 @@
 #
 # This script launches nginx and nginx-agent
 #
-echo "------ version 2025.06.26.2 ------"
+echo "------ version 2025.06.26.3 ------"
 
 # copy initial file to the empy volume, in case of being empty
 cp -p --no-clobber /nginx-initial-config/* /etc/nginx/
