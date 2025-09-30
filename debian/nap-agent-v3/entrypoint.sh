155952967bea3a75b943fd0809acce73db552b77
nergalex <nergalex@hotmail.com>
2025-09-30 14:50:35 +0200

    remove nginx-agent.conf init

--- debian/nap-agent-v3/entrypoint.sh
+++ debian/nap-agent-v3/entrypoint.sh
@@ -2,7 +2,7 @@
 #
 # This script launches nginx and nginx-agent
 #
-echo "------ version 2025.09.26.1 ------"
+echo "------ version 2025.09.30.1 ------"
 
 # copy initial file to the empy volume, in case of being empty
 cp -p --no-clobber /nginx-initial-config/* /etc/nginx/
